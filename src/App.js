5f62e85398d3ec486ebf3a97259b4a0774844f42
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-06-02 13:21:08 +0900

    세팅 및 db 표시

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import DayList from "./components/Daylist";
+import Day from "./components/Day";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Voca App
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <DayList />
+      <Day />
     </div>
   );
 }

96b2a5119b2a0a68d0e43ace38860605cbae101a
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-06-03 16:12:53 +0900

    route 설정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Header from "./components/Header";
 import DayList from "./components/Daylist";
 import Day from "./components/Day";
+import EmptyPage from "./components/EmptyPage";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <DayList />
-      <Day />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<DayList />} />  
+          <Route path="/day/:day" element={<Day />} />
+          <Route path="*" element={<EmptyPage />}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

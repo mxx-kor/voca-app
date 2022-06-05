80c81d4d10ffaf261f6a3c06dbaf0f6b20179511
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-06-05 15:49:03 +0900

    json-server 이용해 data를 가져옴, 배열의 길이로 로딩 표시

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,21 +1,22 @@
-import dummy from "../db/data.json";
 import { useParams } from "react-router-dom";
 import Word from "./Word";
+import useFetch from "../hooks/useFetch";
 
 export default function Day() {
     const day = useParams().day;
-    const wordList = dummy.words.filter(word => word.day === Number(day));
+    const words = useFetch(`http://localhost:3001/words?day=${day}`)
 
     return (
         <>
             <h2>Day {day}</h2>
+            {words.length ? 
             <table>
                 <tbody>
-                    {wordList.map(word => (
+                    {words.map(word => (
                         <Word word={word} key={word.id} />
                     ))}
                 </tbody>
-            </table>
+            </table> : <strong>Loading...</strong>}
         </>
     )
 }
--- src/components/Daylist.js
+++ src/components/Daylist.js
@@ -1,14 +1,16 @@
-import dummy from "../db/data.json"
 import { Link } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 
 export default function DayList() {
+    const days = useFetch("http://localhost:3001/days")
+
     return (
         <ul className="list_day">
-            {dummy.days.map(day => (
+            {days.length ? days.map(day => (
                 <li key={day.id}>
                     <Link to={`/day/${day.day}`}>Day {day.day}</Link>
                 </li>
-            ))}
+            )): <strong>Loading...</strong>}
         </ul>
     );
 }

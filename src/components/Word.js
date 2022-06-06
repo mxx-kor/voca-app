56595d5739c80c40cade49deff77e73e9fe26d3f
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-06-06 16:13:31 +0900

    직접 json파일을 수정하는  PUT, DELETE 기능 구현

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,13 +1,42 @@
 import { useState } from "react"
 
-export default function Word({ word }) {
+export default function Word({ word: w }) {
+    const [word, setWord] = useState(w)
     const [toggle, setToggle] = useState(false);
     const [isDone, setIsDone] = useState(word.isDone);
     const toggleShow = () => {
         setToggle(!toggle)
     }
-    const toggleDone = () => {
-        setIsDone(!isDone)
+    const toggleDone = () => {     
+        fetch(`http://localhost:3001/words/${word.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type' : 'application/json', 
+            },
+            body: JSON.stringify({
+                ...word,
+                isDone: !isDone,
+            })
+        })
+        .then(res => {
+            if(res.ok) {
+                setIsDone(!isDone);
+            }
+        })
+    }
+    const del = () => {
+        if(window.confirm('삭제 하시겠습니까?')) {
+            fetch(`http://localhost:3001/words/${word.id}`, {
+                method: 'DELETE'
+            }).then(res => {
+                if(res.ok) {
+                    setWord({ id: 0 });
+                }
+            });
+        }
+    }
+    if (word.id === 0) {
+        return null;
     }
 
     return (
@@ -19,7 +48,7 @@ export default function Word({ word }) {
             <td>{toggle && word.kor}</td>
             <td>
                 <button onClick={toggleShow}>뜻 {toggle ? '숨기기' : '보기'}</button>
-                <button className="btn_del">삭제</button>
+                <button onClick={del} className="btn_del">삭제</button>
             </td>
         </tr>
     )

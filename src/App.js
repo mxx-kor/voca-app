5566ac85fcabf0a8a6545e79ec0267f978d454cd
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-06-07 18:35:48 +0900

    단어 추가 페이지 세팅

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import DayList from "./components/Daylist";
 import Day from "./components/Day";
 import EmptyPage from "./components/EmptyPage";
+import CreateWord from "./components/CreateWord";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<DayList />} />  
           <Route path="/day/:day" element={<Day />} />
+          <Route path="/create_word" element={<CreateWord />} />
           <Route path="*" element={<EmptyPage />}/>
         </Routes>
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom"
+import useFetch from "../hooks/useFetch"
 
 export default function Header() {
+    const days = useFetch('http://localhost:3001/days');
+
     return (
         <div className="header">
             <h1>
                 <Link to="/">영단어(고급)</Link>
             </h1>
             <div className="menu">
-                <a href="#x" className="link">
+                <Link to="/create_word" className="link">
                     단어추가
-                </a>
+                </Link>
                 <a href="#x" className="link">
                     Day 추가
                 </a>
